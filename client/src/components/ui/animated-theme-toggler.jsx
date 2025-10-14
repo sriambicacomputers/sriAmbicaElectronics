c27cdef60c0682016e0666f6dfdd878c33c764ec
Chakradhar Kommoju <chakrikommoju123@gmail.com>
2025-10-14 21:07:05 +0530

    edit localStorage theme item name

--- client/src/components/ui/animated-theme-toggler.jsx
+++ client/src/components/ui/animated-theme-toggler.jsx
@@ -36,7 +36,7 @@ export const AnimatedThemeToggler = ({
         const newTheme = !isDark
         setIsDark(newTheme)
         document.documentElement.classList.toggle("dark")
-        localStorage.setItem("theme", newTheme ? "dark" : "light")
+        localStorage.setItem("vite-ui-theme", newTheme ? "dark" : "light")
       })
     }).ready
 
